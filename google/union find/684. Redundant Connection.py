88af337308b144759994ba3cbc886c50cdb5d4b8
xiangning qi <xiangniq@amazon.com>
2020-12-23 23:15:55 -0800

    update

--- google/union find/684. Redundant Connection.py
+++ google/union find/684. Redundant Connection.py
@@ -1,2 +1,26 @@
 684. Redundant Connection
 685. Redundant Connection II
+
+I
+给定一个无向图，remove其中一个edge，使得图是个树。
+用union find，当一个边使得两个node在同一个union时候，就返回那个edge
+时空复杂度是N和N
+
+II
+很复杂。。。
+There are two cases for the tree structure to be invalid.
+1) A node having two parents;
+   including corner case: e.g. [[4,2],[1,5],[5,2],[5,3],[2,4]]
+2) A circle exists
+If we can remove exactly 1 edge to achieve the tree structure, a single node can have at most two parents. So my solution works in two steps.
+
+1) Check whether there is a node having two parents. 
+    If so, store them as candidates A and B, and set the second edge invalid. 
+2) Perform normal union find. 
+    If the tree is now valid 
+           simply return candidate B
+    else if candidates not existing 
+           we find a circle, return current edge; 
+    else 
+           remove candidate A instead of B.
+
